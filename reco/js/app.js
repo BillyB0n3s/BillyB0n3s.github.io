84eea435ae585dba7dcdc805398dbf11fd48cd58
BillyB0n3s <67594816+BillyB0n3s@users.noreply.github.com>
2026-04-09 19:39:17 +0200

    Add files via upload

---
+++ reco/js/app.js
@@ -0,0 +1,39 @@
+function getOS() 
+{
+  var userAgent = window.navigator.userAgent, platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'], windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+
+  if (macosPlatforms.indexOf(platform) !== -1) { return 'Mac OS'; }
+  else if (iosPlatforms.indexOf(platform) !== -1) { return 'iOS'; }
+  else if (windowsPlatforms.indexOf(platform) !== -1) { return 'Windows'; }
+  else if (/Android/.test(userAgent)) { return 'Android'; }
+  else if (/Linux/.test(platform)) { return 'Linux'; }
+  return null;
+}
+
+//alert(getOS());
+
+function redirect(currentURL)
+{
+  //alert("copied url : " + currentURL); 
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const nbParams = urlParams.size;
+  const referrer = urlParams.get("referrer");
+  //const param2 = urlParams.get("param2");
+
+  //alert("nbParams: " + nbParams +  ", param1: " + param1 + ", param2: " + param2);
+
+//  if(param1 != null){ alert(param1) };
+//  if(param2 != null){ alert(param2) };
+
+  const os = getOS();
+  if (os == "Android"){ 
+   location.replace('https://play.google.com/store/apps/details?id=com.gboutin.reco&referrer=' + referrer);
+   //location.replace('https://play.google.com/store/apps/details?id=com.gboutin.reco');
+  }else if (os == "iOS") {
+   //location.replace('https://www.apple.com/store');  // https://apps.apple.com/us/app/app_name/app_id ???
+  }
+}
